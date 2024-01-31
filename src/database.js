740d58bd08be79f273a83a31f602cc35de41cf76
diegotech14 <diegoduarte8343@gmail.com>
2024-01-31 09:52:05 -0600

    8 deploy

--- src/database.js
+++ src/database.js
@@ -1,12 +1,11 @@
 import { createPool } from "mysql2/promise";
-import dotenv from 'dotenv';
-dotenv.config()
+import { DB_HOST,DB_NAME,DB_PASSWORD,DB_PORT,DB_USER } from "./config.js";
 
 //The object for connect the database will take the enviroment variables 
 export const pool = createPool({
-    host:process.env.HOST,
-    user:process.env.USER,
-    password:process.env.PASSWORD,
-    port:process.env.PORT,
-    database:process.env.DATABASE, 
+    host:DB_HOST,
+    user:DB_NAME,
+    password:DB_PASSWORD,
+    port:DB_PORT,
+    database:DB_USER, 
 })
